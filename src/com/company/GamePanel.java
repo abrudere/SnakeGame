87e575ada2d34e8a20af2583c9723644cbc8f7ca
Agnese Brudere <agnesebrudere@gmail.com>
2022-05-30 20:23:47 +0300

    Initial commit

---
+++ src/com/company/GamePanel.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel() {
+
+    }
+    //methods we need
+    public void startGame() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+    public void move() {
+
+    }
+    public void checkApple() {
+
+    }
+    public void checkCollisions() {
+
+    }
+    public void gameOver() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
